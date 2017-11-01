9262d8a852c294c5868610d2b5deaf5b628bcec8
Richie Wong <richiewong07@yahoo.com>
2017-10-31 22:31:00 -0500

    updated

---
+++ Promise/chaining.js
@@ -0,0 +1,30 @@
+// Using request-promise and fs-promise modules build a function called saveWebPage which takes two parameters, url and filename. The function should chain the two promises together to download the URL and then save the file.
+
+var rp = require("request-promise");
+var fs = require("fs-promise");
+
+var urls = [
+  'https://en.wikipedia.org/wiki/Futures_and_promises',
+  'https://en.wikipedia.org/wiki/Continuation-passing_style',
+  'https://en.wikipedia.org/wiki/JavaScript',
+  'https://en.wikipedia.org/wiki/Node.js',
+  'https://en.wikipedia.org/wiki/Google_Chrome'
+];
+
+function saveWebPage(url, filename) {
+  rp(url)
+    .then(function (response) {
+      console.log(response);
+      return response;
+    })
+    .then(function(response) {
+      var p = fs.writeFile('./testing/' + filename + '.html', response);
+      console.log('Webpage is saved!');
+      return p;
+    })
+    .catch(function (error) {
+      console.log('You received an error!');
+    })
+}
+
+saveWebPage(urls[0], 'saveWebPage');
