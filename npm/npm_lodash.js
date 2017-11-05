72b6c6594b7376f45311e0731df9960f8a581370
Richie Wong <richiewong07@yahoo.com>
2017-11-05 17:19:55 -0600

    updated

---
+++ npm/npm_lodash.js
@@ -0,0 +1,9 @@
+// Lodash is a swiss army knife toolbelt of a module. For example, there's a shuffle built in, and if you had known that, you could have used it for black jack and memory game.
+//
+// Write a simple program that makes use of lodash's shuffle function.
+
+var lodash = require('lodash');
+
+var list = [1, 2, 3 ,4 ,5 ,6 ,7, 8, 9, 10];
+
+console.log(lodash.shuffle(list))
---
+++ npm/npm_marked.js
@@ -0,0 +1,5 @@
+// Install the marked module, require it, and use the module to convert some hard-coded markdown to HTML, you may use the example code provided in marked's project page.
+
+
+var marked = require('marked');
+ 
